2d39cc637eda8ff94bdd7ea6f2bed26301ce3609
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-10-21 15:50:06 +0300

    path fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const querystring = require('querystring');
 
 module.exports = {
     GET: {
-        '/(.+?)': function (request) {
+        '(.+?)': function (request) {
             if (request.headers['cookie']) {
                 request.cookies = querystring.parse(request.headers['cookie']);
             }
