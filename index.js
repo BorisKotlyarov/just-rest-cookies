d995983bc9defb755fe29262c1c80fa4a054351e
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-10-21 14:08:39 +0300

    fix

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const querystring = require('querystring');
 
 module.exports = {
     GET: {
-        '/(.+?)': function(request){
-            if (request.headers['cookie']){
+        '/(.+?)': function (request) {
+            if (request.headers['cookie']) {
                 request.cookies = querystring.parse(request.headers['cookie']);
             }
+            return;
         }
     }
 };
